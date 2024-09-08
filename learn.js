ea7973e27462f926fbc32590cbdc35e53f792e8a
Dani <174685774+itsonlydaniiii@users.noreply.github.com>
2024-09-08 20:46:54 +0200

    add log between challenge

--- learn.js
+++ learn.js
@@ -674,3 +674,25 @@ function whileLoopDoubler (array) {
 forLoopDoubler([1, 2, 3]); // => [2,4,6]
 whileLoopDoubler([1, 2, 3]); //=> [2,4,6]
 
+
+//Log Between Challenge
+/*Define a function logBetween(lowNum, highNum) that will print every number from lowNum to highNum, inclusive. 
+Inclusive means that the range includes lowNum and highNum.
+Hint: this function only needs to print using console.log it does not need to return.*/
+function logBetween(lowNum, highNum) {
+  for (let i = lowNum; i <= highNum; i += 1) {
+    console.log(i);
+  }
+}
+function logBetweenWhile(lowNum, highNum) {
+  let i = lowNum;
+  while (i <= highNum) {
+    console.log(i);
+    i++;
+  }
+}
+
+
+//ARRAYS
+//store multiple values all within a single structure - like a list almost
+//can hold strings, integers and even other arrays
